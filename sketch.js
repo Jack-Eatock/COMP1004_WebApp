16a5c28b5e720a7fd93ab4d310844d17b21c5ef5
Jack Eatock <46260379+Jack-Eatock@users.noreply.github.com>
2021-02-10 15:15:53 +0000

    Code Optimisation + Comments

--- sketch.js
+++ sketch.js
@@ -1,76 +1,74 @@
-// Main GamePlay vars
-var RateDecresePerFrameE = 0.017;
-var RateDecresePerFrameH = 0.018;
-var RateDecresePerFrameS = 0.026;
-var RateDecresePerFrameSY = 0.01;
+///////// Main GamePlay vars \\\\\\\\\
+var RateDecresePerFrameE = 0.017; var RateDecresePerFrameH = 0.018;
+var RateDecresePerFrameS = 0.026; var RateDecresePerFrameSY = 0.01;
 
-// Movement variables \\
-var playerMovement = {  x : 0, y : 0, };
-var playerSpeed = 4;
+///////// Movement variables \\\\\\\\\
+var PlayerMovement = {  x : 0, y : 0, };
+var PlayerSpeed = 4;
 var CurrentPlayerPos = { x: 600,   y : 300  }; // Start pos, also stores player pos
 
-// UI
+///////// UI \\\\\\\\\
 var PlayerNotice = "Welcome to isolation!";
 
-StudyPercent = { p : 100,
-  set percent(val){ if (val < 0 ){ this.p = 0 } else if (val > 100){this.p = 100;} else {this.p = val;}},
-  get percent (){return this.p;}}
-
-HungerPercent = { p : 100,
-  set percent(val){ if (val < 0 ){ this.p = 0 } else if (val > 100){this.p = 100;} else {this.p = val;}},
-  get percent (){return this.p;}}
-
-EnergyPercent = { p : 100,
-  set percent(val){ if (val < 0 ){ this.p = 0 } else if (val > 100){this.p = 100;} else {this.p = val;}},
-  get percent (){return this.p;}}
-
-SanityPercent = { p : 100,
-  set percent(val){ if (val < 0 ){ this.p = 0 } else if (val > 100){this.p = 100;} else {this.p = val;}},
-  get percent (){return this.p;}}
-
-
-// Collision variables \\
-var playerColiderOffsets = 
-{ 
-  XTopLeftOffset   : -30, YTopLeftOffset    : 15, 
-  XTopRightOffset  :  30, YTopRightOffset   : 15, 
-  XBotLeftOffset   : -30, YBotLeftOffset    : 60, 
-  XBotrightOffset  :  30, YBotrightOffset   : 60, 
+// Defines the functionality of a percent obj
+class Percent{
+  constructor(){this._percent = 100;}
+  get percent(){return this._percent;}
+  set percent(val){if (val < 0 ){ this._percent = 0 } else if (val > 100){this._percent = 100;} else {this._percent = val;}}
 }
 
-// Animation
+// Now simple create the variables.
+var StudyPercent  = new Percent; var HungerPercent = new Percent;
+var EnergyPercent = new Percent; var SanityPercent = new Percent;
+
+///////// Collision variables \\\\\\\\\
+var playerColiderOffsets = 
+{ 
+   XTopLeftOffset   : -30, YTopLeftOffset    : 15, 
+   XTopRightOffset  :  30, YTopRightOffset   : 15, 
+   XBotLeftOffset   : -30, YBotLeftOffset    : 60, 
+   XBotrightOffset  :  30, YBotrightOffset   : 60, 
+}
+
+///////// Animation \\\\\\\\\
 var AnimTimers = [];
-var animationIdentity = 0;
+var AnimationIdentity = 0;
 
-// Loading rooms
+///////// Loading rooms \\\\\\\\\
 var Images = []
-var currentRoom = 0;
+var CurrentRoom = 0;
 
-// Triggers
-var curTrigEvent = 0;
+///////// Triggers \\\\\\\\\
+var CurTrigEvent = 0;
 
-var modeDebug = false;
+var ModeDebug = false;
 var GameBeingPlayed = 0;
 
-// Animating
-var curFrame = 0; // iterates every frame, updates tickClock every 60. (a second)
-var tickClock = 0; // will keep track of seconds, reset every minute. ( 60 secs)
-var curHour = 0;
+///////// Animating \\\\\\\\\
+var CurFrame = 0; // iterates every frame, updates tickClock every 60. (a second)
+var TickClock = 0; // will keep track of seconds, reset every minute. ( 60 secs)
+var CurHour = 0;
 
-// Bedroom Vars
-var isPlantAlive = true;
+///////// Bedroom Vars \\\\\\\\\
+var IsPlantAlive = true;
 
-// Showing SIngle Image On screen. E.g notes / text
-var imageToShow = 0;
-var isOnlyImageShowing = false;
+///////// Showing SIngle Image On screen. E.g notes / text \\\\\\\\\
+var ImageToShow = 0;
+var IsOnlyImageShowing = false;
 
 
-
+/** 
+ * Preload all the data into a easily accessed variable.
+ */
 function preload() {
   data = loadJSON('Maps.json');  // loads required data
 }
 
-
+/** 
+ * - Grab all the references to the images that need to be loaded.
+ * - Loop through each reference, preloading the image into a temp variable
+ * - Add the ready loaded image into an easily accessable array.
+ */
 function SetupImages(){
   var imagesToLoad = data.ImagesToPreload;
   for (var x = 0; x < imagesToLoad.length; x++){
@@ -80,7 +78,12 @@ function SetupImages(){
 }
 
 
-
+/** 
+ * Any functionality to run before the main program. 
+ * - Prepare all the images
+ * - Set the framerate
+ * - Create the canvas
+ */
 function setup() {
   SetupImages()     // prepares all images
   imageMode(CENTER);
@@ -92,7 +95,10 @@ function setup() {
 
 }
 
-
+/** 
+ * Runs every frame.
+ * Basically runs everything.
+ */
 function draw() {
   // Set background to black.
   background(0);
@@ -102,10 +108,10 @@ function draw() {
   // Debug Tools \\
   
   // If only displaying image. Display the image and break out.
-  if (isOnlyImageShowing){
+  if (IsOnlyImageShowing){
     DisplayOnlyImageUntillInput();
     GetInput();
-    if (!modeDebug){DrawUI();}
+    if (!ModeDebug){DrawUI();}
     return;
   }
   
@@ -123,28 +129,33 @@ function draw() {
       break;     
   }
 
-  if (!modeDebug){DrawUI();}
+  if (!ModeDebug){DrawUI();}
 
   // Notifications
   UpdateText(3, 'Notice : ' + PlayerNotice);
   
   // Time stuff
-  curFrame++; // Keep track of frame
-  tickClock = curFrame / 60; // Convert current frame into Secodns.
-  curHour   = (tickClock / 7.5).toFixed(2); // 8 hours per minute
+  CurFrame++; // Keep track of frame
+  TickClock = CurFrame / 60; // Convert current frame into Secodns.
+  CurHour   = (TickClock / 7.5).toFixed(2); // 8 hours per minute
 
 }
 
-
+/** 
+ * - Should be run when the player presses the interact key
+ * - Check what current event is ready to activate.
+ * - Run the event accordingly.
+ */
 function PlayerTriedToInteract(){
 
   // Check which object is currently interactable.
-  switch (curTrigEvent) {
+  switch (CurTrigEvent) {
     case (0):  break; // Nothin interactable.
     case (1):  StudyPercent.percent  += 10; break; // Nothin interactable.
     case (2):  EnergyPercent.percent += 10; break; // Nothin interactable.
-    case (3):  isOnlyImageShowing = true; break; // Nothin interactable.
-    case (4):  isPlantAlive = false; Images[23] = Images[24]; break; // Make plant dead.
+    case (3):  IsOnlyImageShowing = true; break; // Nothin interactable.
+    case (4):  IsPlantAlive = false; Images[23] = Images[24]; break; // Make plant dead.
+    case (5):  CharacterInteraction(); break; // Char interaction
   
     default:
       break;
@@ -152,12 +163,11 @@ function PlayerTriedToInteract(){
 
 }
 
-function DisplayOnlyImageUntillInput(){
-  image(Images[26] , width/2  , 300 );
-}
-
-
-
+/** 
+ * - Should run if the player's collider collides with an interactable object
+ * - Set the text appropriately to the event that can be activated. 
+ * - Display yellow outline around object
+ */
 function PlayerCouldInteract(event){
   var startText = "Press SPACE to INTERACT with : ";
   switch (event) {
@@ -165,40 +175,62 @@ function PlayerCouldInteract(event){
     case (1): image(Images[20] , 476  , 200 );  PlayerNotice = startText + "Computer!"; break; // Pc
     case (2): image(Images[22] ,  453 ,  466 ); PlayerNotice = startText + "Bed!"; break; // Bed
     case (3): image(Images[21] , 366.5  , 292 ); PlayerNotice = startText + "Notes!"; break; // Notes
-    case (4): if (isPlantAlive == true){ image(Images[25] , 599  ,176 );}  PlayerNotice = startText + "Plant!"; break;
-      break; // Nothin interactable.
+    case (4): if (IsPlantAlive == true){ image(Images[25] , 599  ,176 );}  PlayerNotice = startText + "Plant!"; break;
+    case (5): image(Images[27] , 366.5  , 292 );  break; // Char interaction
     
     default:
       break;
   }
 }
 
+/** 
+ * Should run when the player tries to interact with one of their flat mates.
+ */
+function CharacterInteraction(){
 
+}
 
-// Debug function \\
-function CheatTeleportToRoom(active)
-{
-  if (!active){return;}
-  if (keyIsDown(49)) {currentRoom = 1; } 
-  if (keyIsDown(50)) {currentRoom = 2; } 
-  if (keyIsDown(51)) {currentRoom = 3; } 
-  if (keyIsDown(52)) {currentRoom = 4; } 
-  if (keyIsDown(53)) {currentRoom = 5; } 
-  if (keyIsDown(54)) {currentRoom = 6; } 
-  if (keyIsDown(55)) {currentRoom = 0; } 
+/** 
+ * Used when you want only one image or set of images to display
+ * without displaying the room etc. For example displaying the notes.
+ * - Maybe make it so you can inspect objects? 
+ */
+function DisplayOnlyImageUntillInput(){
+  image(Images[26] , width/2  , 300 );
 }
 
 
-// Runs every frame.
+/** 
+ *    [DEBUG FUNCTION]
+ *  - Allow the player to press the number keys and switch rooms easily
+ */
+function CheatTeleportToRoom(active)
+{
+  if (!active){return;} // I don't think a switch can be used in this case :(
+  if (keyIsDown(49)) {CurrentRoom = 1; } 
+  if (keyIsDown(50)) {CurrentRoom = 2; } 
+  if (keyIsDown(51)) {CurrentRoom = 3; } 
+  if (keyIsDown(52)) {CurrentRoom = 4; } 
+  if (keyIsDown(53)) {CurrentRoom = 5; } 
+  if (keyIsDown(54)) {CurrentRoom = 6; } 
+  if (keyIsDown(55)) {CurrentRoom = 0; } 
+}
+
+
+/** 
+ * This function:
+ *  - Retrieves all data required to draw the current room
+ *  - Loops through the data, displaying them accordingly.
+ */
 function DrawRoom(){
 
   //if (keyIsDown(65)){ currentRoom = 1 ;}
-  if (modeDebug){UpdateText(1, 'CurrentRoom ' + currentRoom)};
+  if (ModeDebug){UpdateText(1, 'CurrentRoom ' + CurrentRoom)};
 
   // DISPLAY STATIC IMAGES \\
 
   // First we need to obtain a reference to all the Images we should load and their positions.
-  var ObjToDraw =  data.rooms[currentRoom].ObjectsToDraw;  
+  var ObjToDraw =  data.rooms[CurrentRoom].ObjectsToDraw;  
   var currObj;
 
   // Loop through each object to draw.
@@ -226,11 +258,16 @@ function DrawRoom(){
 
 
 
-
+/** 
+ * This function:
+ *  - Grabs the required data for the animations that should be drawn
+ *  - Manages each animation running, storing the time since last frame for each
+ *  - Then displays the correct frame and updates the time running for each anim.
+ */
 function DisplayAnimations(){
 
   // DISPLAY ANIMATIONS \\
-  var animationsToDraw = data.rooms[currentRoom].AnimationsToDraw;
+  var animationsToDraw = data.rooms[CurrentRoom].AnimationsToDraw;
   var curAnim; var timeNeeded4NextFrame;  var curFrame;
 
  
@@ -255,19 +292,18 @@ function DisplayAnimations(){
      * each frame has a duration, check if the last
      */
 
-
     curFrame = curAnim.Frames[AnimTimers[anim].frame];
     timeNeeded4NextFrame = AnimTimers[anim].timeOfLastFrame + curFrame.Duration;
 
-    if ( tickClock >= timeNeeded4NextFrame){ // Switch to next frame.
+    if ( TickClock >= timeNeeded4NextFrame){ // Switch to next frame.
       // Check if last frame. ADD THIS
       if ( AnimTimers[anim].frame + 1 >= curAnim.Frames.length){
         AnimTimers[anim].frame = 0;
-        AnimTimers[anim].timeOfLastFrame = tickClock;
+        AnimTimers[anim].timeOfLastFrame = TickClock;
       }
       else{
         AnimTimers[anim].frame++; // Iterate frame 
-        AnimTimers[anim].timeOfLastFrame = tickClock; // update time of last frame.
+        AnimTimers[anim].timeOfLastFrame = TickClock; // update time of last frame.
         curFrame = curAnim.Frames[AnimTimers[anim].frame];
       }
     }
@@ -281,9 +317,14 @@ function DisplayAnimations(){
 
 
 
-
+/** 
+ * In charge of switching level / room :
+ * - First actually switch the level/room
+ * - Then move the character either to the specified location passed through
+ * - or if not specified place them in their default location for that room in data.
+ */
 function SwitchLevel(roomNum, x = 0, y = 0){
-  currentRoom = roomNum; // Changes the room that is being displayed.
+  CurrentRoom = roomNum; // Changes the room that is being displayed.
 
   // If spawn location is not specifically set use default
   if (x == 0 && y == 0){
@@ -299,7 +340,13 @@ function SwitchLevel(roomNum, x = 0, y = 0){
 
 
 
-
+/** 
+ * Checks if the player is colliding with any colliders / triggers
+ * - Takes the current position, calculates the 4 points for the player colider
+ * - Loops through each collider for the room they are in
+ * - Checking if the player is colliding with any, if they are react accordingly
+ * - Based on the type of collider. Some prevent movement, Act as event triggers etc
+ */
 function IsColliding(PosX, PosY, Offsets)
 {
   let CordsToCheck =
@@ -313,7 +360,7 @@ function IsColliding(PosX, PosY, Offsets)
   var PointY;
 
  // Now we know where the player will be. Calculate if the new pos is overlapping any colliders.
- var Cols =  data.rooms[currentRoom].Colliders;  // Grab the colliders for this room. Optimsie later!
+ var Cols =  data.rooms[CurrentRoom].Colliders;  // Grab the colliders for this room. Optimsie later!
  var Colliding = false;     
  var eventTrigFlag = false; // Ensures a new event trig is found. Or set default.
  var eventToStart =  0;
@@ -339,9 +386,9 @@ function IsColliding(PosX, PosY, Offsets)
   }
  }
    // If no trig event found reset
-   if (!eventTrigFlag){curTrigEvent = 0; PlayerNotice = "null";}
+   if (!eventTrigFlag){CurTrigEvent = 0; PlayerNotice = "null";}
    else{ // There was an event found, Make sure only one is set.
-     curTrigEvent = eventToStart;
+     CurTrigEvent = eventToStart;
      PlayerCouldInteract(eventToStart);
    }
 
@@ -356,10 +403,13 @@ function IsColliding(PosX, PosY, Offsets)
 
 
 
-
+/** 
+ * - Take in the userinput
+ * - Move the character accordingly, taking into account any collisions.
+ */
 function MoveCharacter()
 {
-  if (modeDebug){ UpdateText(2, 'Nx' + CurrentPlayerPos.x + ' Ny' + CurrentPlayerPos.y);}
+  if (ModeDebug){ UpdateText(2, 'Nx' + CurrentPlayerPos.x + ' Ny' + CurrentPlayerPos.y);}
  
   var xAddition = 0; // X Change in movement
   var yAddition = 0; // Y .. 
@@ -368,50 +418,50 @@ function MoveCharacter()
   // Might change later, basically keeps movement speed the same even when both up and left etc is pressed at the same time. Ideally would use Normalised Vector3 For direction.
 
   // Check if it is only X axis
-  if (playerMovement.x != 0 && playerMovement.y == 0)     { xAddition =  playerSpeed * playerMovement.x; }
+  if (PlayerMovement.x != 0 && PlayerMovement.y == 0)     { xAddition =  PlayerSpeed * PlayerMovement.x; }
   // Check if it is only Y axis
-  else if(playerMovement.x == 0 && playerMovement.y != 0) { yAddition = playerSpeed * playerMovement.y; }
+  else if(PlayerMovement.x == 0 && PlayerMovement.y != 0) { yAddition = PlayerSpeed * PlayerMovement.y; }
   // Check if it is noth X and Y axis
   else{
-    yAddition =   Math.round  (( playerSpeed / 1.6 )) * playerMovement.y;
-    xAddition =   Math.round  (( playerSpeed / 1.6 )) * playerMovement.x;
+    yAddition =   Math.round  (( PlayerSpeed / 1.6 )) * PlayerMovement.y;
+    xAddition =   Math.round  (( PlayerSpeed / 1.6 )) * PlayerMovement.x;
   }
   // Determines if the Character will colide if it moves. If not move!
   if (!IsColliding(CurrentPlayerPos.x + xAddition, CurrentPlayerPos.y + yAddition, playerColiderOffsets)) {
     CurrentPlayerPos.x += xAddition;
     CurrentPlayerPos.y += yAddition;
-    if (modeDebug){UpdateText(0,"Collision : false")}  
-  } else{ if (modeDebug){UpdateText(0,"Collision : true")}}
+    if (ModeDebug){UpdateText(0,"Collision : false")}  
+  } else{ if (ModeDebug){UpdateText(0,"Collision : true")}}
 
   image(Images[0] , CurrentPlayerPos.x  , CurrentPlayerPos.y ); 
 }
 
 
-
-
-
+/** 
+ * Reads player input
+ */
 function GetInput() {
 
   // No input should work, other than space to close the image
-  if (isOnlyImageShowing){
-    if (keyIsDown(69)){ isOnlyImageShowing = false;}
+  if (IsOnlyImageShowing){
+    if (keyIsDown(69)){ IsOnlyImageShowing = false;}
     return;
   }
 
   // Keycode 65 = a   Keycode 87 = s
-  if (keyIsDown(65) && keyIsDown(68))  {  playerMovement.x =  0; }
+  if (keyIsDown(65) && keyIsDown(68))  {  PlayerMovement.x =  0; }
   else{
-    if (keyIsDown(65)) {  playerMovement.x = -1; } 
-    if (keyIsDown(68)) {  playerMovement.x =  1; } 
-    if (!keyIsDown(65) && !keyIsDown(68) ) { playerMovement.x = 0;}
+    if (keyIsDown(65)) {  PlayerMovement.x = -1; } 
+    if (keyIsDown(68)) {  PlayerMovement.x =  1; } 
+    if (!keyIsDown(65) && !keyIsDown(68) ) { PlayerMovement.x = 0;}
   }
   
   // keycode 68 = d   Keycode 83 = w
-  if (keyIsDown(87) && keyIsDown(83))  {  playerMovement.y =  0; }
+  if (keyIsDown(87) && keyIsDown(83))  {  PlayerMovement.y =  0; }
   else{
-    if (keyIsDown(87)) {  playerMovement.y =  -1; }
-    if (keyIsDown(83)) {  playerMovement.y =   1; }
-    if (!keyIsDown(87) && !keyIsDown(83) ) { playerMovement.y = 0;}
+    if (keyIsDown(87)) {  PlayerMovement.y =  -1; }
+    if (keyIsDown(83)) {  PlayerMovement.y =   1; }
+    if (!keyIsDown(87) && !keyIsDown(83) ) { PlayerMovement.y = 0;}
   }
 
   // Player tries to Interact
@@ -422,7 +472,10 @@ function GetInput() {
 
 
 
-
+/** 
+ * Simple method to update on screen text. 
+ * Makes it easier to update font size etc without changing each.
+ */
 function UpdateText(ref, newText){
   // Default vals
   noStroke(0); fill(255); textSize(20); textAlign(LEFT);
@@ -439,7 +492,10 @@ function UpdateText(ref, newText){
 
 
 
-
+/** 
+ * Painfully long but actually simple method to draw all the ui
+ * Usefull in making slight changes to the ui.
+ */
 function DrawUI(){
 
     // Apply the change per frame to each variable bar.
@@ -478,8 +534,8 @@ function DrawUI(){
     textSize(18);
     text("- - - - ]|I{•------» ⓢΔ几𝕚Ⓣｙ «------•}I|[ - - - -", width/2 , 75);
 
-    var hour = Math.floor(curHour);
-    var minute = Math.floor((curHour - hour) * 60);
+    var hour = Math.floor(CurHour);
+    var minute = Math.floor((CurHour - hour) * 60);
     textAlign(LEFT);
     text("Time [ " + hour + " : " + minute + " ]",      98 , 578);
     text("[02/03/2020] ", 100 , 600);
